6dfb783b6d8f6986fa451f182089aa2ea9fa5045
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-02-25 11:06:56 -0800

    First Barrister Redis implementation

---
+++ barrister-redis.gemspec
@@ -0,0 +1,25 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'barrister-redis/version'
+
+Gem::Specification.new do |spec|
+  spec.name          = "barrister-redis"
+  spec.version       = Barrister::Redis::VERSION
+  spec.authors       = ["Erin Swenson-Healey"]
+  spec.email         = ["erin.swenson.healey@gmail.com"]
+  spec.summary       = %q{Redis transport and server-container for Barrister.}
+  spec.description   = %q{Redis transport and server-container for Barrister.}
+  spec.homepage      = ""
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -z`.split("\x0")
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.5"
+  spec.add_development_dependency "rake"
+  spec.add_dependency 'redis', '~> 3.0'
+  spec.add_dependency 'barrister', '~> 0'
+end
---
+++ lib/barrister-redis.rb
@@ -0,0 +1,80 @@
+require "barrister-redis/version"
+require 'barrister'
+require 'redis'
+require 'pry'
+
+module Barrister
+
+  module Transports
+
+    class Redis
+
+      def initialize(list_name, database_url)
+        @list_name = list_name
+        @client = ::Redis.connect url: database_url
+      end
+
+      def request(message)
+        # reply-to tells the server where we'll be listening
+        request = {
+          'reply_to' => 'reply-' + SecureRandom.uuid,
+          'message'  => message
+        }
+
+        # insert our request at the head of the list
+        @client.lpush(@list_name, JSON.generate(request))
+
+        # pop last element off our list in a blocking fashion
+        channel, response = @client.brpop(request['reply_to'], timeout=30)
+
+        JSON.parse(response)['message']
+      end
+
+    end
+
+  end
+
+  module Containers
+
+    class Redis
+
+      def initialize(json_path, database_url, list_name, handlers=[])
+        @list_name = list_name
+
+        # establish connection to Redis
+        @client = ::Redis.connect url: database_url
+
+        # initialize service
+        contract = Barrister::contract_from_file(json_path)
+        @server  = Barrister::Server.new(contract)
+
+        # register each provided handler
+        handlers.each do |(interface_name, interface_klass)|
+          @server.add_handler interface_name, interface_klass.new
+        end
+
+      end
+
+      def start
+        while true
+          # pop last element off our list in a blocking fashion
+          channel, request = @client.brpop(@list_name)
+
+          parsed = JSON.parse request
+
+          # reverse the message we were sent
+          response = {
+            'message' => @server.handle(parsed['message'])
+          }
+
+          # 'respond' by inserting our reply at the head of a 'reply'-list
+          @client.lpush(parsed['reply_to'], JSON.generate(response))
+        end
+
+      end
+
+    end
+
+  end
+
+end
